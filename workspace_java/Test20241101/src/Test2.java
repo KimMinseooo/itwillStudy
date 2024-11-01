14f0dd20f4b5eea8b23207c170cd1b26e4479ac4
Minseo Kim <minseo.dev@gmail.com>
2024-11-01 12:58:11 +0900

    수업 내용 저장

--- workspace_java/Test20241101/src/Test2.java
+++ workspace_java/Test20241101/src/Test2.java
@@ -25,8 +25,28 @@ public class Test2 {
 			System.out.println(age+"세의 입장료 : 2000원");
 		} else if (age <20){
 			System.out.println(age+"세의 입장료 : 2500원");
-		} else {
+		} else if(age<60){
 			System.out.println(age+"세의 입장료 : 3000원");
+		} else { 
+			System.out.println(age+"세의 입장료 : 0원");
 		}
+		
+		//정수형 변수 score 선언 초기값 점수 저장(0~100) 
+//		=> 학점 100~90 A, 89 ~ 80 B, 79 ~70 C, 69~ 60 D, 나머지 F
+		int score = 70 ; 
+			if(score>=90) {
+				System.out.println("학점 : A");
+			} else if (score>=80) {
+				System.out.println("학점 : B");
+			} else if (score>=70) {
+				System.out.println("학점 : C");
+			} else if (score>=60) {
+				System.out.println("학점 : D");
+			} else {
+				System.out.println("학점 : F");
+			}
+		
+		
+		
 	}
 }
