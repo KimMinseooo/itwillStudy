37d540052036af3dfe7d0e7e68457e0a3196aaf4
Minseo Kim <minseo.dev@gmail.com>
2024-12-23 12:52:31 +0900

    BACKEND - Spring - (SpringTest3_Mybatis)
    프로젝트 초기 설정 및
    Mybatis 를 위한 라이브러리 설정

---
+++ BACKEND/JSP/workspace_spring/SpringTest3_Mybatis/src/main/java/com/itwillbs/test3_mybatis/StudentDTO.java
@@ -0,0 +1,40 @@
+package com.itwillbs.test3_mybatis;
+// STUDENT 테이블에 대응하는 StudentDTO 클래스 정의 
+public class StudentDTO {
+	// 멤버변수 선언 (= 컬럼명) 
+	private int idx;
+	private String name;
+	
+	public StudentDTO() {}  // 기본생성자
+
+	public StudentDTO(int idx) {  // idx를 받는 생성자
+		this.idx=idx;
+	}
+	
+	public StudentDTO(int idx, String name) {   // idx, name을 받는 생성자
+		this.idx = idx;
+		this.name = name;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "StudentDTO [idx=" + idx + ", name=" + name + "]";
+	}
+	
+	
+}
---
+++ BACKEND/JSP/workspace_spring/SpringTest3_Mybatis/src/main/java/com/itwillbs/test3_mybatis/vo/StudentVO.java
@@ -0,0 +1,12 @@
+package com.itwillbs.test3_mybatis.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data		// @Getter @Setter @RequiredArgsConstructor @ToString 등
+@AllArgsConstructor    // 파라미터 생성자 추가(단, 기본생성자 필요에 따라 @NoArgsConstructor)
+public class StudentVO {
+	private int idx;
+	private String name;
+	
+}
