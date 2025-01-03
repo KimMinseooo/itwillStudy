aa8d80dbad035d2702b2a026c28da33134462414
Minseo Kim <minseo.dev@gmail.com>
2025-01-03 17:38:57 +0900

    BACKEND - new Project(Member) 생성

---
+++ BACKEND/JSP/workspace_spring/Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/mapper/MemberMapper.java
@@ -0,0 +1,10 @@
+package com.itwillbs.mvc_board.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.itwillbs.mvc_board.vo.MemberVO;
+
+@Mapper
+public interface MemberMapper {
+	int registMember(MemberVO member);
+}
