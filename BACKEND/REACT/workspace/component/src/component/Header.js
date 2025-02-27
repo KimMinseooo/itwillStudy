b0c912057490abfe46ba994d2fe070120c9d809d
Minseo Kim <minseo.dev@gmail.com>
2025-02-27 11:00:25 +0900

    BACKEND -React 수업내용 저장

---
+++ BACKEND/REACT/workspace/component/src/component/Header.js
@@ -0,0 +1,14 @@
+
+function Header () {
+    // const 변수 = ''; // 멤버변수
+    // const 함수명 = () => {} // 메서드
+
+    return (
+        <header>
+            <h1>header</h1>
+        </header>
+    );
+}
+
+// 다른파일에서 사용할 수 있도록 내보내기 
+export default Header;
